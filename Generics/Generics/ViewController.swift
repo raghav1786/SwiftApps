8008b03edf64762c61c58774de9ae01c1be372c8
Raghav Sharma <53406407+raghav1786@users.noreply.github.com>
2019-12-21 11:25:20 +0530

    added base project
    consist of ViewController. Main StoryBoard and other necessary files required at beginning of project

---
+++ Generics/Generics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by RAGHAV SHARMA on 21/12/19.
+//  Copyright © 2019 RAGHAV SHARMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
