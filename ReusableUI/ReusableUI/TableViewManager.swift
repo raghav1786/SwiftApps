c177af9e1735f7b8f3541d42ffd89e06d1318373
Raghav <222raghav@gmail.com>
2019-12-24 21:40:48 +0530

    added tableViewManager
    will be used for custom cells.

---
+++ ReusableUI/ReusableUI/TableViewManager.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewManager.swift
+//  ReusableUI
+//
+//  Created by RAGHAV SHARMA on 24/12/19.
+//  Copyright © 2019 RAGHAV SHARMA. All rights reserved.
+//
+
+import Foundation
+
+public protocol TableViewCompatible {
+    
+}
