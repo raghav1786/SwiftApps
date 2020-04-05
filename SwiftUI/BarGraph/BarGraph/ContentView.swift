2355a19769b5a61a7d36997fafe05e71068d202e
Raghav <222raghav@gmail.com>
2020-04-05 23:33:46 +0530

    animation using barGraph
    -learned rotate
    -default
    -zoom animations
    and showing data in form of bar graph in swiftUI

---
+++ SwiftUI/BarGraph/BarGraph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarGraph
+//
+//  Created by RAGHAV SHARMA on 02/04/20.
+//  Copyright © 2020 RAGHAV SHARMA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        BarGraph()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
