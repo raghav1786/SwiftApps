8f2f32658596b1c89108eaec5cbf026c4cfd1f47
222raghav <222raghav@gmail.com>
2021-06-04 00:35:23 +0530

    added Utility classes
    - added activity indicator
    - added font util and color util for the app
    - added imageview extension to load images in background from url String

---
+++ Gallery/Gallery/Main/Application/Utils/ColorUtility/ColorUtility.swift
@@ -0,0 +1,16 @@
+import UIKit
+extension UIColor {
+    public class var backgroundColor: UIColor {
+            return #colorLiteral(red: 0.6507557034, green: 0.9253898263, blue: 0.8449390531, alpha: 1)
+    }
+    public class var appRed: UIColor {
+            return #colorLiteral(red: 1, green: 0.0915768519, blue: 0.1535034478, alpha: 1)
+    }
+}
+
+
+extension CGColor {
+    public class var suggestionBackground: CGColor {
+        return #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).cgColor
+    }
+}
