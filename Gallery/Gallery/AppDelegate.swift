1201831f41800f0ab2a8f5413436a3fbc6401921
222raghav <222raghav@gmail.com>
2021-06-05 01:24:33 +0530

    added launch screen text
    - added launch screen in did resign and did become active.

--- Gallery/Gallery/AppDelegate.swift
+++ Gallery/Gallery/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var launchScreen : UIViewController!
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let home = HomeBuilder().buildHomeModule()
@@ -36,6 +37,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let splash = launchScreen{
+            splash.view.removeFromSuperview()
+        }
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Hide Protected UI
+        launchScreen = UIStoryboard.init(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+        window?.addSubview(self.launchScreen.view)
     }
 
 
--- Gallery/Gallery/SceneDelegate.swift
+++ Gallery/Gallery/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var launchScreen : UIViewController!
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -39,11 +39,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        if let splash = launchScreen{
+            splash.view.removeFromSuperview()
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        launchScreen = UIStoryboard.init(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+        window?.addSubview(self.launchScreen.view)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
