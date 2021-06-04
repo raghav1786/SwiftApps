9764db2315dda289c0487d1810ba7c325dad092a
222raghav <222raghav@gmail.com>
2021-06-04 23:53:55 +0530

    fixed file name for noDataInteractor

--- Gallery/Gallery/Main/Application/Components/NoData/Builder/NoDataBuilder.swift
+++ Gallery/Gallery/Main/Application/Components/NoData/Builder/NoDataBuilder.swift
@@ -1,7 +1,7 @@
 class NoDataBuilder {
     func buildNoDataModule(noDataModel: NoDataUIModel) -> ComponentView {
         let viewController = NoDataViewController()
-        let interactor = NoCityInteractor(noDataModel: noDataModel)
+        let interactor = NoDataInteractor(noDataModel: noDataModel)
         let presenter = NoDataPresenter(view: viewController, interactor: interactor)
         viewController.presenter = presenter
         return ComponentView(view: viewController)
