a40ff2e7d902b9ad1ac5c9f9ac6dab4694684dff
222raghav <222raghav@gmail.com>
2021-06-18 12:21:04 +0530

    code optimization, adding mark methods
    - module :- movie details

--- BookMyShow/BookMyShow/Main/Application/Components/MovieDetails/ReusableCell/MovieDetailsCardCell/MovieDetailsCardCell.swift
+++ BookMyShow/BookMyShow/Main/Application/Components/MovieDetails/ReusableCell/MovieDetailsCardCell/MovieDetailsCardCell.swift
@@ -1,10 +1,17 @@
-
 import UIKit
-
 class MovieDetailsCardCell: UIView {
-    @IBOutlet weak var titleLbl: UILabel!
-    @IBOutlet weak var dateRelease: UILabel!
-    @IBOutlet weak var votes: UILabel!
-    @IBOutlet weak var duration: UILabel!
-    @IBOutlet weak var descOverview: UILabel!
+    //MARK:- Outlets
+    @IBOutlet weak private var titleLbl: UILabel!
+    @IBOutlet weak private var dateRelease: UILabel!
+    @IBOutlet weak private var votes: UILabel!
+    @IBOutlet weak private var duration: UILabel!
+    @IBOutlet weak private var descOverview: UILabel!
+    
+    //MARK:- Custom Method
+    func configureCell(model:MovieDetailsCardUIModel) {
+        titleLbl.text = model.movieTitle
+        descOverview.text = model.movieDesc
+        dateRelease.text = model.releaseDate
+        votes.text = "\(String(describing: model.votes ?? 0.0))"
+    }
 }
