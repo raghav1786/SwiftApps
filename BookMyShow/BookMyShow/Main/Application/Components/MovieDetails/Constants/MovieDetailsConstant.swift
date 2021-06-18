2cc019e34d332eadb5494771eac791d296a3224a
222raghav <222raghav@gmail.com>
2021-06-18 12:19:11 +0530

    added constants files specfic to folder structure.
    - extension constants
    - static data constants

---
+++ BookMyShow/BookMyShow/Main/Application/Components/MovieDetails/Constants/MovieDetailsConstant.swift
@@ -0,0 +1,6 @@
+import UIKit
+struct MovieDetailsConstant {
+    static let movieDetailsTitle = "Movie Details"
+    static let movieCardBottomConstraint = CGFloat(20.0)
+    static let movieCardBottomConstraintSwipe = CGFloat(50.0)
+}
---
+++ BookMyShow/BookMyShow/Main/Application/Utils/Extensions/ExtensionConstants.swift
@@ -0,0 +1,5 @@
+import Foundation
+struct BounceEffect {
+    static let key = "position"
+    static let keyPath = "position.y"
+}
