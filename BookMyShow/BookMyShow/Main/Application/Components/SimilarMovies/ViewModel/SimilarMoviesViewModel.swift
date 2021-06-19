c593262d0fe07883e626a5bd06adff94985ad64a
222raghav <222raghav@gmail.com>
2021-06-19 16:12:49 +0530

    set similar movies top title with movie name

--- BookMyShow/BookMyShow/Main/Application/Components/SimilarMovies/ViewModel/SimilarMoviesViewModel.swift
+++ BookMyShow/BookMyShow/Main/Application/Components/SimilarMovies/ViewModel/SimilarMoviesViewModel.swift
@@ -2,12 +2,12 @@ import Foundation
 class SimilarMoviesViewModel {
     private var moviesRepository = MoviesRepository()
     var movieList : [Movie]?
-    var selectedMovieID: Int64?
+    var selectedMovie: Movie?
     
     func getSimilarMovieList(completion : @escaping(Bool) -> ()) {
         DispatchQueue.global().async { [weak self] in
             guard let strongSelf = self else { return }
-            guard let selectedMovieID = strongSelf.selectedMovieID
+            guard let selectedMovieID = strongSelf.selectedMovie?.id
             else { return }
             strongSelf.moviesRepository.getSimilarMovieList(movieID: selectedMovieID) { movies,error in
                 DispatchQueue.main.async {
