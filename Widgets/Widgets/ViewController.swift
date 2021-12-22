07379ec28717a90b4e6d721f3b87bf774197799d
222raghav <222raghav@gmail.com>
2021-12-22 22:50:50 +0530

    added Widgets Project

---
+++ Widgets/Widgets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Widgets
+//
+//  Created by RAGHAV SHARMA on 22/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
